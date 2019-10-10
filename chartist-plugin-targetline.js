6e018d705d8f789f42736915bdf6bc57325ac0dc
Christian J. Schweinhardt <are.u.kidding@me.com>
2019-10-10 12:53:31 +0200

    implement optional label

--- chartist-plugin-targetline.js
+++ chartist-plugin-targetline.js
@@ -17,11 +17,15 @@
     root['Chartist.plugins.ctTargetLine'] = factory(Chartist);
   }
 }(this, function (Chartist) {
-  (function(window, document, Chartist) {
+  (function(_window, document, Chartist) {
     'use strict';
     var defaultOptions = {
       class: 'ct-target-line',
       value: null,
+      axis: 'Y', // TODO: implement horizontally target line
+      showLabel: true,
+      labelClass: 'ct-target-line-label',
+      label: 0,
     };
     Chartist.plugins = Chartist.plugins || {};
     Chartist.plugins.ctTargetLine = function(options) {
@@ -32,7 +36,30 @@
           }
           chart.on('created', function (context) {
             var targetLineY = projectY(context.chartRect, context.bounds, options.value || Math.abs(context.bounds.low));
-            context.svg.elem('line', {
+            if (!options.value && context.bounds.low === 0) {
+              return;
+            }
+            if (options.showLabel) {
+              const labelsGroup = context.svg.querySelector('.ct-labels');
+              const height = 30; // TODO: calculate based on options
+              const width = 30; // TODO: calculate based on options
+              const content = document.createElement('span');
+              content.className = `ct-label ct-vertical ct-start ${options.labelClass}`;
+              content.setAttribute('xmlns', Chartist.namespaces.xhtml);
+              content.innerText = options.label;
+              content.style = `width: ${width}px; height: ${height}px;`;
+              labelsGroup.foreignObject(
+                content,
+                Chartist.extend({ style: 'overflow: visible;' }, {
+                  y: targetLineY - Math.round(height / 2),
+                  x: context.chartRect.padding.left + chart.options.axisY.labelOffset.x,
+                  height,
+                  width,
+                })
+              );
+            }
+            const gridsGroup = context.svg.querySelector('.ct-grids');
+            gridsGroup.elem('line', {
               x1: context.chartRect.x1,
               x2: context.chartRect.x2,
               y1: targetLineY,
